4be172ccc6d44c51681366cdeb059a4d1968c369
GaurAfro <gauraboschma@gmail.com>
2023-08-05 16:20:49 +0200

    Added nvim and 2 themes

---
+++ neovim.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Build prerequisites
+sudo nala install ninja-build gettext cmake unzip curl
+
+# Move to Repository directory
+cd ~/Repository
+
+# clone
+git clone https://github.com/neovim/neovim
+cd neovim
+git checkout stable
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
+
+# Retrieve https://github.com/junegunn/vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    
+cp -r ./resources/nvim ~/.config/
+nvim
